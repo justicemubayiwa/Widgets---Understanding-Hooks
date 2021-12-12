5511e19869287510be05f6e4cbf9c491842df4f7
justicemubayiwa <j.mubayiwa@gmail.com>
2021-12-12 14:24:15 -0500

    setTimeout to avoid query searches on every key press

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,21 +2,66 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const Search=()=> {
-    const [term, setTerm] = useState('')
+    const [term, setTerm] = useState('sharks')
+    const [results, setResults] = useState([])
 
     useEffect(()=>{
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php')
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: {term},
+                }
+            })
+            
+            setResults(data.query.search)
         }
+
+        const timeoutId = setTimeout(()=> {
+            if (term) {
+                search()
+            }
+        },500)
+
+        return ()=>{
+            clearTimeout(timeoutId)
+        }        
+    }, [term])
+
+    const renderedResults = results.map((result)=> {
+        return  (
+            <div key={result.pageid} className='item'>
+                <div className='right floated content'>
+                    <a 
+                        href={`https://en.wikipedia.org/wiki/${result.title}`} 
+                        className='ui button'>Go
+                    </a>
+                </div>
+                <div className='content'>
+                    <h1 className='header'>
+                        {result.title}
+                    </h1>
+                    <div>
+                        <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+                    </div>
+                </div>
+            </div>
+        )
     })
 
     return (
         <div className="ui form">
            <div className="field">
                <label>Enter Search Term</label>
-               <input onChange={e=> setTerm(e.target.value)} 
+               <input value={term} onChange={e=> setTerm(e.target.value)} 
                 className="input"
                 />
+           </div>
+           <div className='ui celled list'>
+                {renderedResults}
            </div>
         </div>
     )
