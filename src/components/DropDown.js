011a739ae2bab568d976cf0b65e9d836e4387aba
justicemubayiwa <j.mubayiwa@gmail.com>
2021-12-13 12:10:25 -0500

    hide and show options list onClick

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const DropDown = ({options, selected, onSelectedChange}) => {
+    const [open, setOpen] = useState(false)
+
     const renderedOptions = options.map(option => {
+        if (option.value === selected.value){
+            return null;
+        }
+
         return (
             <div 
                 key={option.value}
@@ -16,10 +22,13 @@ const DropDown = ({options, selected, onSelectedChange}) => {
         <div className='ui form'>
             <div className='field'>
                 <label className ='label'>Select Color</label>
-                <div className='ui selection dropdown visible active'>
+                <div 
+                    onClick={()=>setOpen(!open)} 
+                    className={`ui selection dropdown ${ open ? 'visible active' : ''}`}
+                >
                     <i className='dropdown icon'></i>
                     <div className='text'>{selected.label}</div>
-                    <div className='menu visible transition'>
+                    <div className={`menu ${ open ? 'visible transition' : ''}`}>
                         {renderedOptions}
                     </div>
                 </div>
