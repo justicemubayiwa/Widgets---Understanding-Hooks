6973d649e8fc353b71f96091d29bf63dcbf0a8b1
justicemubayiwa <j.mubayiwa@gmail.com>
2021-12-13 20:18:47 -0500

    rerender convert component with useEffect when language or text value changes

---
+++ src/components/Convert.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react'
+
+const Convert = ({language, text}) => {
+    useEffect(()=> {
+
+    },[language, text])
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Convert
