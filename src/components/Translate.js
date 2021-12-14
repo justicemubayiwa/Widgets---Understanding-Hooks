88e0f78290962f490af700a7d857ad7539f5ad2c
justicemubayiwa <j.mubayiwa@gmail.com>
2021-12-13 20:10:23 -0500

    Text input field

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -16,12 +16,28 @@ const options = [
     },
 ]
 
-const Translate = () => {
-const [language, setLanguage] = useState(options[0])
+const Translate = (e) => {
+    const [language, setLanguage] = useState(options[0])
+    const [text, setText] = useState('')
 
     return (
         <div>
-            <Dropdown selected={language} onSelectedChange={setLanguage} options={options}/>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input 
+                        value={text} 
+                        onChange={(e)=> setText(e.target.value)}  
+                    />
+                </div>     
+            </div>
+            
+            <Dropdown
+                label="Select A Language" 
+                selected={language} 
+                onSelectedChange={setLanguage} 
+                options={options}
+            />
         </div>
     )
 }
